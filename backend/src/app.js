8dd912c0af136be6cfc898a83c47950c6b126f5b
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-11 21:50:49 +0500

    configured middlewares in express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,18 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
+const app = express();
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
-const app = express()
-
-
-
-
-
-
-
-export {app};
+export { app };
