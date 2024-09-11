2ace1d005bcc5a41dbb9e47c0cbb229052be542b
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-11 21:04:00 +0500

    added cloudinary utility

---
+++ backend/src/utlis/cloudinary.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+    const response = await cloudinary.uploader.upload(filePath, {resource_type: "auto",});
+    console.log("File uploaded successfully!");
+    return response;
+  } catch (error) {
+    console.log("File uploading failed in cloudinary", error);
+  }
+};
