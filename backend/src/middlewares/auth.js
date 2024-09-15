698cef2e2c47de0069ffda1596a9218017a79ef6
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-15 20:51:59 +0500

    resolved issue of "no response going towards postman when error occurs"

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -6,19 +6,19 @@ const isAuthenticated = async (req, res, next) => {
   try {
     const token = req?.cookies?.accessToken;
     if (!token) {
-      throw new ApiError(401, "unauthorized request");
+      next( new ApiError(401, "unauthorized request"))
     }
 
     let decodedInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedInfo._id);
     if (!user) {
-      throw new ApiError("Invalid access Token", 401);
+      next( new ApiError("Invalid access Token", 401))
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(400, "User Authentication failed!", error);
+    next( new ApiError(400, "User Authentication failed!", error))
   }
 };
 
