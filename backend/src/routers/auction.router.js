fd8f5401e1e67101d85f0c57928d2278a4e70805
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-16 16:01:38 +0500

    added isAuthenticated middleware in addNewAuctionItem route.

--- backend/src/routers/auction.router.js
+++ backend/src/routers/auction.router.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { addNewAuctionItem } from "../controllers/auction.controller.js";
+import { isAuthenticated } from "../middlewares/auth.js"
 
 const router = Router();
-router.post("/add-new-auction-Item",addNewAuctionItem);
+router.post("/add-new-auction-Item",isAuthenticated,addNewAuctionItem);
 
 export default router;
