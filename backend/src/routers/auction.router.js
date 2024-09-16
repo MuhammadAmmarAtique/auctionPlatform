268aa9e797daa0789879b6e6b66901381e06d6f6
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-16 23:54:26 +0500

    used authorization middleware in add new auction item route to stop bidder to add new auction item

--- backend/src/routers/auction.router.js
+++ backend/src/routers/auction.router.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { addNewAuctionItem } from "../controllers/auction.controller.js";
-import { isAuthenticated } from "../middlewares/auth.js"
+import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 
 const router = Router();
-router.post("/add-new-auction-Item",isAuthenticated,addNewAuctionItem);
+router.post(
+  "/add-new-auction-Item",
+  isAuthenticated,
+  isAuthorized("Auctioneer"),
+  addNewAuctionItem
+);
 
 export default router;
