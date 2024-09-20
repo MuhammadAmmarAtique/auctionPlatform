3b0112739a5fb4f8ceeb135c5cd83cb49cda6dc2
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-20 16:09:01 +0500

    added auth middlewares in calculateComission route

--- backend/src/routers/commissionProof.router.js
+++ backend/src/routers/commissionProof.router.js
@@ -1,7 +1,17 @@
 import Router from "express";
 import { calculateComission } from "../controllers/commissionProof.controller.js";
+import {
+  isAuthenticated,
+  isAuthorized,
+} from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
-router.post("/calculate-comission", calculateComission);
+router.post(
+  "/calculate-comission",
+  isAuthenticated,
+  isAuthorized("Auctioneer"),
+  calculateComission
+);
 
 export default router;
