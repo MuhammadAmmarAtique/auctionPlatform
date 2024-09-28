fad1ed27ef2c4b5bf0a76ad5b22fbe3d72ebc2dc
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-28 21:35:06 +0500

    created userslice & added logout reducers

---
+++ frontend/src/store/slices/userSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    loading: false,
+    isAuthenticated: false,
+    user: {},
+    leaderboard: []
+  },
+  reducers: {
+    //logout reducers
+    logoutSuccess(state, action) {
+      state.isAuthenticated = false,
+        state.user = {};
+    },
+    logoutFailed(state, action) {
+      state.loading = false,
+        state.isAuthenticated = state.isAuthenticated,
+        state.user = state.user;
+    },
+    clearAllErrors(state, action) {
+      state.loading = false,
+        state.isAuthenticated = state.isAuthenticated,
+        state.user = state.user,
+        state.leaderboard = state.leaderboard;
+    }
+  }
+});
+
+export const logout = () => async (dispatch) => {
+  try {
+    const response = await axios.get("http://localhost:3000/api/v1/users/logout", { withCredentials: true });
+    dispatch(userSlice.actions.logoutSuccess());
+    toast.success(response.data.message);
+    dispatch(userSlice.actions.clearAllErrors());
+  } catch (error) {
+    dispatch(userSlice.actions.logoutFailed());
+    toast.success(error.response.data.message);
+    dispatch(userSlice.actions.clearAllErrors());
+  }
+};
+
+export default userSlice.reducer;
