454547d3cf1f6e66f48a49cc70eb83f7c89c4018
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-29 14:56:39 +0500

    formatted file

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -30,18 +30,18 @@ const userSlice = createSlice({
     //logout reducers
     logoutSuccess(state, action) {
       state.isAuthenticated = false,
-        state.user = {};
+      state.user = {};
     },
     logoutFailed(state, action) {
       state.loading = false,
-        state.isAuthenticated = state.isAuthenticated,
-        state.user = state.user;
+      state.isAuthenticated = state.isAuthenticated,
+      state.user = state.user;
     },
     clearAllErrors(state, action) {
       state.loading = false,
-        state.isAuthenticated = state.isAuthenticated,
-        state.user = state.user,
-        state.leaderboard = state.leaderboard;
+      state.isAuthenticated = state.isAuthenticated,
+      state.user = state.user,
+      state.leaderboard = state.leaderboard;
     }
   }
 });
@@ -75,7 +75,7 @@ export const logout = () => async (dispatch) => {
     dispatch(userSlice.actions.clearAllErrors());
   } catch (error) {
     dispatch(userSlice.actions.logoutFailed());
-    toast.success(error.response.data.message);
+    toast.error(error.response.data.message);
     dispatch(userSlice.actions.clearAllErrors());
   }
 };
