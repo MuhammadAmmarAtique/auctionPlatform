0bec2a368c248380809da52774ca91de772ab65a
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-03 01:04:00 +0500

    logging error for debugging

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -18,6 +18,13 @@ const errorHandler = (err, req, res, next) => {
     errors: [],
     data: null
   });
+  
+console.error(`[Error] - ${new Date().toISOString()}: An error occurred in the backend application`);
+console.err(`err: ${err}`);
+console.error(`Error Message: ${err.message}`);
+console.error(`Stack Trace: ${err.stack}`);
+
+  
 };
 
 export default errorHandler;
