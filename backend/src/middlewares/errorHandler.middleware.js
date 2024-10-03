ae9b2cf72caa768f5fd2e308131026ea3bd138e0
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-03 14:37:24 +0500

    handling error related to mongoose validation + added comments above every block of code

--- backend/src/middlewares/errorHandler.middleware.js
+++ backend/src/middlewares/errorHandler.middleware.js
@@ -1,6 +1,7 @@
 import { ApiError } from "../utlis/ApiError.js";
 
 const errorHandler = (err, req, res, next) => {
+   // ApiError (for custom errors thrown manually)
   if (err instanceof ApiError) {
     return res.status(err.statusCode).json({
       success: false,
@@ -9,15 +10,25 @@ const errorHandler = (err, req, res, next) => {
       data: err.data,
     });
   }
+   // Mongoose Validation Error
+   if (err.name === 'ValidationError') {
+    const messages = Object.values(err.errors).map((error) => error.message);
+    return res.status(400).json({
+      success: false,
+      message: "Validation Error",
+      errors: messages, // Send all validation errors
+    });
+  }
 
-  // For other, unexpected errors
+   // General Error (fallback for unexpected errors)
   res.status(500).json({
     success: false,
     message: "Internal Server Error",
     errors: [],
     data: null,
   });
-
+  
+  // Error logging for debugging
   console.error(
     `[Error] - ${new Date().toISOString()}: An error occurred in the backend application`
   );
