2ddad534df1e09591e55bda8c02971452e68004e
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-04 21:23:55 +0500

    created submitComissionProofSlice with submitComissionProof functionality for Auctioneer

---
+++ frontend/src/store/slices/submitComissionProofSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const submitComissionProofSlice = createSlice({
+  name: "submitComissionProof",
+  initialState: {
+    loading: false,
+  },
+  reducers: {
+    submitComissionProofRequest(state, action) {
+      state.loading = true;
+    },
+    submitComissionProofSuccess(state, action) {
+      state.loading = false;
+    },
+    submitComissionProofFailed(state, action) {
+      state.loading = false;
+    },
+  },
+});
+
+export const submitComissionProof = (data) => async (dispatch) => {
+  dispatch(submitComissionProofSlice.actions.submitComissionProofRequest());
+  try {
+    const response = await axios.post(
+      "http://localhost:3000/api/v1/comissionProofs/proofOfComission",
+      data,
+      {
+        withCredentials: true,
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    dispatch(submitComissionProofSlice.actions.submitComissionProofSuccess());
+    toast.success(response.data.message);
+  } catch (error) {
+    dispatch(submitComissionProofSlice.actions.submitComissionProofFailed());
+    toast.error(error.response.data.message);
+  }
+};
+
+export default submitComissionProofSlice.reducer;
