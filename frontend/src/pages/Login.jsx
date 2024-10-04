4844e31e4d1785ca71299131fb1b45a27eb6e5b6
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-04 15:27:08 +0500

    styled login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,27 +27,41 @@ const Login = () => {
 
   return (
     <>
-      <form onSubmit={handleLogin}>
-        <input
-          autoComplete="your-email"
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />{" "}
-        <br />
-        <input
-          autoComplete="your-current-password"
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />{" "}
-        <br />
-        <input type="submit" value={loading? "logging..." :"Login" } disabled={loading} />
-      </form>
+      <section className="w-full ml-0 m-0 h-fit px-5 pt-20 lg:pl-[320px] flex flex-col min-h-screen py-4 justify-center">
+        <div className="bg-white mx-auto w-full h-auto px-2 flex flex-col gap-4 items-center py-4 justify-center rounded-md sm:w-[600px] sm:h-[450px]">
+          <h1
+            className={`text-[#d6482b] text-2xl font-bold mb-2 min-[480px]:text-4xl md:text-5xl`}
+          >
+            Login
+          </h1>
+          <form onSubmit={handleLogin} className="flex flex-col gap-5 w-full">
+            <div className="flex flex-col gap-2">
+              <label className="text-[16px] text-stone-500">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="text-[16px] text-stone-500">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="text-[16px] py-2 bg-transparent border-b-[1px] border-b-stone-500 focus:outline-none"
+              />
+            </div>
+            <button
+              className="bg-[#d6482b] font-semibold hover:bg-[#b8381e] transition-all duration-300 text-xl py-2 px-4 rounded-md text-white mx-auto my-4"
+              type="submit"
+            >
+              {loading ? "Logging In..." : "Login"}
+            </button>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
