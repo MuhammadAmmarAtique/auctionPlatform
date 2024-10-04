0c2f8538a0b1cf39bf0154fd39d51df4d638e98b
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-04 19:04:31 +0500

    created index.js file for ease in importing pages in app.jsx

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./home";
+import Signup from "./Signup";
+import Login from "./Login";
+import SubmitComissionProof from "./SubmitComissionProof";
+
+export { Home, Signup, Login, SubmitComissionProof };
