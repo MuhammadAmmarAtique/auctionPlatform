e1e9fe1f9b928a0632aedc478d411075d1bf89b6
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-04 21:24:21 +0500

    added submitComissionProofReducer in redux store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import submitComissionProofReducer from "./slices/submitComissionProofSlice"
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        submitComissionProof:submitComissionProofReducer
     }
 })
