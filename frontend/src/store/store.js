bac3298d22c5ba0a7686e46f117f5f488bb40d3f
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-11 17:10:58 +0500

    added auctionReducer in store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
-import submitComissionProofReducer from "./slices/submitComissionProofSlice"
+import submitComissionProofReducer from "./slices/submitComissionProofSlice";
+import auctionReducer from "./slices/auctionSlice";
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        submitComissionProof:submitComissionProofReducer
-    }
-})
+  reducer: {
+    user: userReducer,
+    submitComissionProof: submitComissionProofReducer,
+    auction:auctionReducer,
+  },
+});
