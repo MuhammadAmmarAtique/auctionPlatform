52bf4d403cbe6c948047d08acee5e972197abbd7
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-16 20:45:12 +0500

    automatically closing sidebar when user will click on any link in less width devices for better user experience

--- frontend/src/layout/SideDrawer.jsx
+++ frontend/src/layout/SideDrawer.jsx
@@ -21,6 +21,7 @@ const SideDrawer = () => {
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(logout());
+    setShow(false); // to automatically close navbar/sidebar in less width devices
   };
 
   return (
@@ -37,7 +38,9 @@ const SideDrawer = () => {
         } transition-all duration-100 p-4 flex flex-col justify-between lg:left-0 border-r-[1px] border-r-stone-500`}
       >
         <div className="relative">
-          <Link to={"/"}>
+          <Link 
+          to={"/"} 
+          onClick={() => setShow(false)}>
             <h4 className="text-2xl font-semibold mb-4">
             Auction<span className="text-[#D6482b]">Platform</span>
             </h4>
@@ -46,6 +49,7 @@ const SideDrawer = () => {
             <li>
               <Link
                 to={"/auctions"}
+                onClick={() => setShow(false)}
                 className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
               >
                 <RiAuctionFill /> Auctions
@@ -54,6 +58,7 @@ const SideDrawer = () => {
             <li>
               <Link
                 to={"/leaderboard"}
+                onClick={() => setShow(false)}
                 className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
               >
                 <MdLeaderboard /> Leaderboard
@@ -64,6 +69,7 @@ const SideDrawer = () => {
                 <li>
                   <Link
                     to={"/submit-commission-proof"}
+                    onClick={() => setShow(false)}
                     className="flex font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
                   >
                     <FaFileInvoiceDollar /> Submit Commission Proof
@@ -72,6 +78,7 @@ const SideDrawer = () => {
                 <li>
                   <Link
                     to={"/create-auction"}
+                    onClick={() => setShow(false)}
                     className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
                   >
                     <IoIosCreate /> Create Auction
@@ -80,6 +87,7 @@ const SideDrawer = () => {
                 <li>
                   <Link
                     to={"/view-my-auctions"}
+                    onClick={() => setShow(false)}
                     className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
                   >
                     <FaEye /> View My Auctions
@@ -91,6 +99,7 @@ const SideDrawer = () => {
               <li>
                 <Link
                   to={"/dashboard"}
+                  onClick={() => setShow(false)}
                   className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
                 >
                   <MdDashboard /> Dashboard
@@ -103,12 +112,14 @@ const SideDrawer = () => {
               <div className="my-4 flex gap-2">
                 <Link
                   to={"/sign-up"}
+                  onClick={() => setShow(false)}
                   className="bg-[#D6482B] font-semibold hover:bg-[#b8381e] text-xl py-1 px-3 rounded-md text-white"
                 >
                   Sign Up
                 </Link>
                 <Link
                   to={"/login"}
+                  onClick={() => setShow(false)}
                   className="text-[#91877f] bg-transparent border-2 border-[#91877f] font-semibold  hover:bg-[#b8381e] hover:text-white hover:border-transparent text-xl py-1 px-4 rounded-md"
                 >
                   Login
@@ -130,6 +141,7 @@ const SideDrawer = () => {
               <li>
                 <Link
                   to={"/me"}
+                  onClick={() => setShow(false)}
                   className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
                 >
                   <FaUserCircle /> Profile
@@ -139,6 +151,7 @@ const SideDrawer = () => {
             <li>
               <Link
                 to={"/how-it-works-info"}
+                onClick={() => setShow(false)}
                 className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
               >
                 <SiGooglesearchconsole /> How it works
@@ -147,6 +160,7 @@ const SideDrawer = () => {
             <li>
               <Link
                 to={"/about"}
+                onClick={() => setShow(false)}
                 className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
               >
                 <BsFillInfoSquareFill /> About Us
@@ -163,12 +177,14 @@ const SideDrawer = () => {
           <div className="flex gap-2 items-center mb-2">
             <Link
               to="/"
+              onClick={() => setShow(false)}
               className="bg-white text-stone-500 p-2 text-xl rounded-sm hover:text-blue-700"
             >
               <FaFacebook />
             </Link>
             <Link
               to="/"
+              onClick={() => setShow(false)}
               className="bg-white text-stone-500 p-2 text-xl rounded-sm hover:text-pink-500"
             >
               <RiInstagramFill />
@@ -176,15 +192,17 @@ const SideDrawer = () => {
           </div>
           <Link
             to={"/contact"}
+            onClick={() => setShow(false)}
             className="text-stone-500 font-semibold hover:text-[#d6482b] hover:transition-all hover:duration-150"
           >
             Contact Us
           </Link>
           <p className="text-stone-500">&copy; Auction Platform, LLC.</p>
           <p className="text-stone-500">
-            Degined By{" "}
+            Designed By{" "}
             <Link
               to={"/"}
+              onClick={() => setShow(false)}
               className="font-semibold hover:text-[#d6482b] hover:transition-all hover:duration-150"
             >
               Team Auction Platform
