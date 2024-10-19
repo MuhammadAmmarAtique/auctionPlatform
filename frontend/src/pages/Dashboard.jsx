f122ec72f6ca73977ea79aa7983ac8dd15f5673d
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-20 00:35:35 +0500

    added Dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import {
+  getAllPaymentProofs,
+  getRegisteredUserCountByMonth,
+  getMonthlyRevenue,
+} from "../store/slices/superAdminSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AuctionItemDelete from "../components/Dashboard/AuctionItemDelete";
+import BiddersAndAuctioneersRegisteredGraph from "../components/Dashboard/BiddersAndAuctioneersRegisteredGraph";
+import RevenueGraph from "../components/Dashboard/RevenueGraph";
+import { PaymentProofs } from "../components/Dashboard/PaymentProofs";
+import Spinner from "../components/Spinner";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.superAdmin);
 
-export default Dashboard
+  useEffect(() => {
+    dispatch(getMonthlyRevenue());
+    dispatch(getRegisteredUserCountByMonth());
+    dispatch(getAllPaymentProofs());
+  }, []);
+
+  const { user, isAuthenticated } = useSelector((state) => state.user);
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    if (user.role !== "Super Admin" || !isAuthenticated) {
+      navigateTo("/");
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="w-full ml-0 m-0 h-fit px-5 pt-20 lg:pl-[320px] flex flex-col gap-10">
+            <h1
+              className={`text-[#d6482b] text-2xl font-bold mb-2 min-[480px]:text-4xl md:text-6xl xl:text-7xl 2xl:text-8xl`}
+            >
+              Dashboard
+            </h1>
+            <div className="flex flex-col gap-10">
+              <div>
+                <h3 className="text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl">
+                  Monthly Total Payments Received
+                </h3>
+                <RevenueGraph />
+              </div>
+              <div>
+                <h3 className="text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl">
+                  Users
+                </h3>
+                <BiddersAndAuctioneersRegisteredGraph />
+              </div>
+              <div>
+                <h3 className="text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl">
+                  Payment Proofs
+                </h3>
+                <PaymentProofs />
+              </div>
+              <div>
+                <h3 className="text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl">
+                  Delete Items From Auction
+                </h3>
+                <AuctionItemDelete />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Dashboard;
