d58bc95f492306fdd830e61e869ea0a22861b63f
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-19 14:41:40 +0500

    added deleteAuctionItem & getAllPaymentProofs methods with reducers

--- frontend/src/store/slices/superAdminSlice.js
+++ frontend/src/store/slices/superAdminSlice.js
@@ -1,9 +1,80 @@
-const { createSlice } = require("@reduxjs/toolkit");
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import { getUserAuctionItems, getAllAuctionItems } from "./auctionSlice";
 
 const superAdminSlice = createSlice({
   name: "superAdmin",
-  initialState: [],
-  reducers: {},
+  initialState: {
+    loading: false,
+    AllPaymentProofs : []
+  },
+  reducers: {
+    // #1 Delete Auction item reducers
+    deleteAuctionItemRequest(state, action) {
+      state.loading = true;
+    },
+    deleteAuctionItemSuccess(state, action) {
+      state.loading = false;
+    },
+    deleteAuctionItemFailed(state, action) {
+      state.loading = false;
+    },
+    // #2 Get All payment Proofs reducers
+    getAllPaymentProofsRequest(state, action) {
+      state.loading = true;
+    },
+    getAllPaymentProofsSuccess(state, action) {
+      state.loading = false;
+      state.AllPaymentProofs = action.payload;
+    },
+    getAllPaymentProofsFailed(state, action) {
+      state.loading = false;
+    },
+    //reducer for clearing errors/ Reset slice
+    clearAllErrors(state, action) {
+      state.loading = false;
+    },
+  },
 });
+
+export const deleteAuctionItem = (id) => async (dispatch) => {
+  dispatch(superAdminSlice.actions.deleteAuctionItemRequest());
+  try {
+    const response = axios.delete(
+      `http://localhost:3000/api/v1/superAdmins/deleteAuctionItem/${id}`,
+      {
+        withCredentials: true,
+      }
+    );
+    dispatch(superAdminSlice.actions.deleteAuctionItemSuccess());
+    dispatch(getUserAuctionItems());
+    dispatch(getAllAuctionItems());
+    toast.success(response.data.message);
+    dispatch(superAdminSlice.actions.clearAllErrors());
+  } catch (error) {
+    dispatch(superAdminSlice.actions.deleteAuctionItemFailed());
+    toast.error(error.response.data.message);
+    dispatch(superAdminSlice.actions.clearAllErrors());
+  }
+};
+
+export const getAllPaymentProofs = () => async (dispatch) => {
+  dispatch(superAdminSlice.actions.getAllPaymentProofsRequest());
+  try {
+    const response = axios.delete(
+      `http://localhost:3000/api/v1/superAdmins/getAllPaymentProofs`,
+      {
+        withCredentials: true,
+      }
+    );
+    dispatch(superAdminSlice.actions.getAllPaymentProofsSuccess(response.data.data));
+    toast.success(response.data.message);
+    dispatch(superAdminSlice.actions.clearAllErrors());
+  } catch (error) {
+    dispatch(superAdminSlice.actions.getAllPaymentProofsFailed());
+    toast.error(error.response.data.message);
+    dispatch(superAdminSlice.actions.clearAllErrors());
+  }
+};
 
 export default superAdminSlice.reducer;
