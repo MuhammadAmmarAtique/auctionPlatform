314fef6f4a3477dc331025fdbaf51bd34e03f8dc
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-10-19 14:20:38 +0500

    added superAdminSlice

---
+++ frontend/src/store/slices/superAdminSlice.js
@@ -0,0 +1,9 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const superAdminSlice = createSlice({
+  name: "superAdmin",
+  initialState: [],
+  reducers: {},
+});
+
+export default superAdminSlice.reducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,12 +3,14 @@ import userReducer from "./slices/userSlice";
 import submitComissionProofReducer from "./slices/submitComissionProofSlice";
 import auctionReducer from "./slices/auctionSlice";
 import bidReducer from "./slices/bidSlice"
+import superAdminReducer from "./slices/superAdminSlice"
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     submitComissionProof: submitComissionProofReducer,
     auction:auctionReducer,
-    bid:bidReducer
+    bid:bidReducer,
+    superAdmin: superAdminReducer
   },
 });
