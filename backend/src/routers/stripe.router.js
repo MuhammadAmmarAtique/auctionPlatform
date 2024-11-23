9ee9b8b381eea9eea481f0c82f041b7957b6b7d8
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-11-23 13:36:07 +0500

    added auth middleware in stripe route

--- backend/src/routers/stripe.router.js
+++ backend/src/routers/stripe.router.js
@@ -1,7 +1,8 @@
 import Router from "express";
 import { createCheckoutSession } from "../controllers/stripe.controller.js";
+import { isAuthenticated, isAuthorized } from "../middlewares/auth.middleware.js"
 const router = Router();
 
-router.route("/create-checkout-session").post(createCheckoutSession);
+router.route("/create-checkout-session").post(isAuthenticated, isAuthorized("Auctioneer"), createCheckoutSession);
 
 export default router;
